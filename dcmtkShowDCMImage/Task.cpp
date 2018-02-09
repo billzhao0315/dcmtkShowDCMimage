dd537e12bf987810e5898c043644d38d02ac77db
Zhao, Bill <bill.zhao@elekta.com>
2018-02-09 18:22:45 +0800

    forgot one cpp file

---
+++ dcmtkShowDCMImage/Task.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "Task.h"
+
+Task::Task()
+{
+    
+}
+
+Task::~Task()
+{
+    
+}
+
+void Task::onDraw(CDC* /*pDC*/)
+{
+    
+}
+
+bool Task::onMouseMBtnDown(UINT /*nFlags*/, const CPoint& /*point*/)
+{
+    return false;
+}
+
+
+bool Task::onMouseWheel(UINT /*nFlags*/, short /*zDelta*/, const CPoint& /*pt*/)
+{
+    return false;
+}
+
+
+void Task::onUpdate(CView* /*pSender*/, LPARAM /*lHint*/, CObject* /*pHint*/)
+{
+    
+}
+
+
--- dcmtkShowDCMImage/TaskManager.h
+++ dcmtkShowDCMImage/TaskManager.h
@@ -1,16 +1,25 @@
 #pragma once
-
+#include <vector>
 #include "Observer.h"
 #include "dcmtkShowDCMImageDoc.h"
 
 class USSTBaseView;
-
+class Task;
 class TaskManager: public Observer
 {
 public:
     TaskManager( USSTBaseView* pView, CdcmtkShowDCMImageDoc* pDocument );
 
 private:
+
+    struct TaskEntry
+    {
+        std::string m_strName;
+        std::shared_ptr<Task> m_pTaskInstance;
+    };
+
+    std::vector< TaskEntry > m_arrTasks;
+
     USSTBaseView* m_pView;
     CdcmtkShowDCMImageDoc* m_pDoc;
 };
