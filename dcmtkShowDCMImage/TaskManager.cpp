32534caac64757f7054a082b134eb44a5c9d4bbe
Zhao, Bill <bill.zhao@elekta.com>
2018-02-11 10:47:25 +0800

    add some function on the taskmanager

--- dcmtkShowDCMImage/TaskManager.cpp
+++ dcmtkShowDCMImage/TaskManager.cpp
@@ -6,3 +6,83 @@ TaskManager::TaskManager(USSTBaseView* pView, CdcmtkShowDCMImageDoc* pDocument)
     m_pView = pView;
     m_pDoc = pDocument;
 }
+
+bool TaskManager::onMouseWheel(UINT /*nFlags*/, short /*zDelta*/, const CPoint& /*pt*/)
+{
+    return false;
+}
+
+bool TaskManager::onMouseLBtnDown(UINT /*nFlags*/, const CPoint& /*point*/)
+{
+    return false;
+}
+
+bool TaskManager::onMouseMBtnDown(UINT /*nFlags*/, const CPoint& /*point*/)
+{
+    return false;
+}
+
+bool TaskManager::onMouseMove(UINT /*nFlags*/, const CPoint& /*point*/)
+{
+    return false;
+}
+
+void TaskManager::onUpdate(CView* /*pSender*/, LPARAM /*lHint*/, CObject* /*pHint*/)
+{
+    
+}
+
+void TaskManager::onDraw(CDC* /*pDC*/)
+{
+    
+}
+
+bool TaskManager::onKeyDown(UINT /*nChar*/, UINT /*nRepCnt*/, UINT /*nFlags*/)
+{
+    return false;
+}
+
+bool TaskManager::onKeyUp(UINT /*nChar*/, UINT /*nRepCnt*/, UINT /*nFlags*/)
+{
+    return false;
+}
+
+bool TaskManager::onMouseLBtnDblClk(UINT /*nFlags*/, const CPoint& /*point*/)
+{
+    return false;
+}
+
+bool TaskManager::onMouseLBtnUp(UINT /*nFlags*/, const CPoint& /*point*/)
+{
+    return false;
+}
+
+bool TaskManager::onMouseLeave(WPARAM /*wParam*/, LPARAM /*lParam*/)
+{
+    return false;
+}
+
+bool TaskManager::onMouseRBtnDown(UINT /*nFlags*/, const CPoint& /*point*/)
+{
+    return false;
+}
+
+bool TaskManager::onMouseRBtnUp(UINT /*nFlags*/, const CPoint& /*point*/)
+{
+    return false;
+}
+
+bool TaskManager::onSetCursor(CWnd* /*pWnd*/, UINT /*nHitTest*/, UINT /*message*/)
+{
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
--- dcmtkShowDCMImage/TaskManager.h
+++ dcmtkShowDCMImage/TaskManager.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include "Observer.h"
 #include "dcmtkShowDCMImageDoc.h"
+#include "TaskType.h"
 
 class USSTBaseView;
 class Task;
@@ -9,6 +10,33 @@ class TaskManager: public Observer
 {
 public:
     TaskManager( USSTBaseView* pView, CdcmtkShowDCMImageDoc* pDocument );
+
+
+    // Keyboard handlers
+
+    virtual bool onKeyDown( UINT nChar, UINT nRepCnt, UINT nFlags );
+    virtual bool onKeyUp( UINT nChar, UINT nRepCnt, UINT nFlags );
+
+    // Mouse handlers
+    virtual bool onMouseLBtnDblClk( UINT nFlags, const CPoint& point );
+    virtual bool onMouseLBtnDown( UINT nFlags, const CPoint& point );
+    virtual bool onMouseLBtnUp( UINT nFlags, const CPoint& point);
+    virtual bool onMouseLeave( WPARAM wParam, LPARAM lParam );
+    virtual bool onMouseMBtnDown( UINT nFlags, const CPoint& point );
+    virtual bool onMouseMove( UINT nFlags, const CPoint& point );
+    virtual bool onMouseRBtnDown( UINT nFlags, const CPoint& point );
+    virtual bool onMouseRBtnUp( UINT nFlags, const CPoint& point );
+    virtual bool onMouseWheel( UINT nFlags, short zDelta, const CPoint& pt );
+    virtual bool onSetCursor(  CWnd* pWnd, UINT nHitTest, UINT message );
+
+    // Update handler
+    virtual void onUpdate( CView* pSender, LPARAM lHint, CObject* pHint );
+
+    // Drawing
+    virtual void onDraw( CDC* pDC );
+
+    virtual void initTasks( const std::vector<Tasktype>& arrEnabledTasks ) = 0;
+
 
 private:
 
